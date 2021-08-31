fc6f5aac25517b8f3158166f049249192c8d3b31
Mariana <57501545+wari99@users.noreply.github.com>
2021-08-30 23:24:57 -0300

    Create RectApp.java

---
+++ RectApp.java
@@ -0,0 +1,42 @@
+public class RectApp {
+    public static void main (String[] args) {
+        Rect r1 = new Rect(1,1,10,10);
+        r1.print();
+      
+        r1.area();     
+      //System.out.format("Area: %d", r1.area());
+      
+        r1.drag(30,50);    
+    }
+}
+class Rect {
+    int x, y;
+    int w, h;
+    Rect (int x, int y, int w, int h) {//PARAMETROS 
+        this.x = x;  
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    } 
+  
+    void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);  
+    }
+	
+	int area(){
+		int areaRect;
+		areaRect = this.w * this.h;
+
+		return areaRect;          
+	}
+
+    void drag(int dx, int dy){
+        dx = dx + x;
+        dy = dy + y;
+      
+      //System.out.format("\nObjeto Arrastado: posicao(%d,%d)", dx, dy );    
+    }
+	
+
+}
