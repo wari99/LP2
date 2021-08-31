26dce47aac5b436c032bba8ad3ad4c1ceb78c148
Mariana <57501545+wari99@users.noreply.github.com>
2021-08-30 23:29:49 -0300

    Update RectApp.java

--- Exs/RectApp.java
+++ Exs/RectApp.java
@@ -12,7 +12,7 @@ public class RectApp {
 class Rect {
     int x, y;
     int w, h;
-    Rect (int x, int y, int w, int h) {//PARAMETROS 
+    Rect (int x, int y, int w, int h) {
         this.x = x;  
         this.y = y;
         this.w = w;
