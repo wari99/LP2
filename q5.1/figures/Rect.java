bfd4644c8c963bc5bb33bded4ce22523df47008b
Mariana <57501545+wari99@users.noreply.github.com>
2021-09-12 22:46:51 -0300

    Update Rect.java

--- q5.1/figures/Rect.java
+++ q5.1/figures/Rect.java
@@ -13,11 +13,6 @@ public class Rect {
         this.h = h;
     }
 
-    public void print () {
-        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
-            this.w, this.h, this.x, this.y);
-    }
-
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         g2d.drawRect(this.x,this.y, this.w,this.h);
