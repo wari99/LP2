cd4e6a7f4bd37ffc5f265254d96f36fd7ba2226c
Mariana <57501545+wari99@users.noreply.github.com>
2021-09-19 23:22:01 -0300

    Create Figure.java

---
+++ PROJETO/13set-17set/figures/Figure.java
@@ -0,0 +1,11 @@
+package figures;
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.Point;
+
+public abstract class Figure {
+	public abstract void paint (Graphics g);
+
+}
