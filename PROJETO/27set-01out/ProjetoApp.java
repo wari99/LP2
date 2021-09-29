bfd186bd419c01eca0491a85c15f30527b664dc4
Mariana <57501545+wari99@users.noreply.github.com>
2021-09-28 22:42:08 -0300

    Create ProjetoApp.java

---
+++ PROJETO/27set-01out/ProjetoApp.java
@@ -0,0 +1,143 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.awt.geom.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import figures.*;
+
+class ProjetoApp {
+    public static void main (String[] args) {
+        ProjetoFrame frame = new ProjetoFrame();
+        frame.setVisible(true);
+    }
+}
+
+class ProjetoFrame extends JFrame {
+	
+	ArrayList<Figure> figs = new ArrayList<Figure>();
+	Random rand = new Random();
+
+	Point posicaodomouse;
+    Figure focus = null;	
+
+	
+    ProjetoFrame () {
+        this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+		
+		
+		this.addMouseListener(
+				new MouseAdapter() {
+				public void mousePressed (MouseEvent evt) { 
+					focus = null;
+					int x = evt.getX();
+					int y = evt.getY();
+					
+					for (Figure figs: figs) { //varrer a lista de figuras
+						if (figs.clicked(x,y)) { //se uma for uma fig no ponto x y 
+							focus = figs; // foco se torna a figura
+							repaint();
+							break;
+						}
+						else {
+							focus = null;
+							repaint();
+						}
+				   }
+				}
+			}
+		);
+		
+		this.addMouseMotionListener(
+			new MouseMotionAdapter(){
+				public void mouseDragged (MouseEvent evt) {
+					for (Figure figs: figs){
+							if (focus == figs) { //se o foco estiver na figura
+								focus.x = evt.getX() - focus.w;
+								focus.y = evt.getY() - focus.h;
+								repaint();								
+							}
+					}
+				}
+			}
+		);
+		
+        this.addKeyListener ( 
+            new KeyAdapter() {
+                public void keyPressed (KeyEvent evt) {
+					Point posicaodomouse = getMousePosition();
+					
+					int w = rand.nextInt(70)+10;
+					int h = rand.nextInt(70)+10;
+					
+					int teclapressionada = evt.getKeyCode();
+					
+					int r1 = rand.nextInt(255);
+					int g1 = rand.nextInt(255);
+					int b1 = rand.nextInt(255);
+					
+					int r2 = rand.nextInt(255);
+					int g2 = rand.nextInt(255);	
+					int b2 = rand.nextInt(255);
+					
+                    int inicioangulo = rand.nextInt(360);
+                    int finalangulo = rand.nextInt(360);			
+
+                    Color contorno = new Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));					
+					
+                    if (evt.getKeyChar() == 'e') {		
+						figs.add(new Ellipse(posicaodomouse.x,posicaodomouse.y,w,h,r1,g1,b1,r2,g2,b2));
+                        repaint();  
+                    }
+					
+					if (evt.getKeyChar() == 'r'){	
+						figs.add(new Rect(posicaodomouse.x,posicaodomouse.y,w,h,r1,g1,b1,r2,g2,b2));
+						repaint(); 
+					}
+					
+                    if (evt.getKeyChar() == 't') {				
+						figs.add(new Triangulo(posicaodomouse.x,posicaodomouse.y,w,h,r1,g1,b1,r2,g2,b2));
+						repaint();
+                    }
+					
+                    if (evt.getKeyChar() == 'y') {
+                        figs.add(new Arco(posicaodomouse.x,posicaodomouse.y,w,h,r1,g1,b1,r2,g2,b2, inicioangulo, finalangulo));
+						repaint();
+					}					
+								
+					
+                }
+            }
+        );
+		
+	
+        this.setTitle("Projeto LP2");
+        this.setSize(550, 550);
+    }
+
+    public void paint (Graphics g) {
+        super.paint(g);
+		
+		for (Figure fig: this.figs){
+			fig.paint(g);
+		}
+    }
+}
+
+
+
+	
