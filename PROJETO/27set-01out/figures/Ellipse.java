7cd65348e1c0bcb8b2b3f879e48b75ecb8046da2
Mariana <57501545+wari99@users.noreply.github.com>
2021-10-02 18:28:24 -0300

    Update Ellipse.java

--- PROJETO/27set-01out/figures/Ellipse.java
+++ PROJETO/27set-01out/figures/Ellipse.java
@@ -4,39 +4,19 @@ import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Ellipse2D.Double;
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-
 public class Ellipse extends Figure{
 			
-	public Ellipse (int x, int y, int w, int h, int r, int g, int b, int rC, int gC, int bC){
-		super(x,y,w,h,r,g,b,rC,gC,bC);
-	
-		this.x = x;
-		this.y = y;
-		this.w = w;
-		this.h = h;
-
-		this.r = r;
-		this.g = g;
-		this.b = b;
-		
-		
-		this.rC = rC;
-		this.gC = gC;
-		this.bC = bC;	
-		
+	public Ellipse (int x, int y, int w, int h, Color contorno, Color fundo){
+		super(x,y,w,h,contorno,fundo);
 	}
-	
 	
 	public void paint (Graphics g){
 		Graphics2D g2d = (Graphics2D) g;
 
-		g2d.setColor(new Color(this.r,this.g,this.b));
+		g2d.setColor(fundo);
 		g2d.fill(new Ellipse2D.Double(this.x, this.y, this.w, this.h));
 		
-		g2d.setColor(new Color(this.rC,this.gC,this.bC));
+		g2d.setColor(contorno);
 		g2d.draw(new Ellipse2D.Double(this.x,this.y,this.w,this.h));
-		
 	}
 }
