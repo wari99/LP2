f747f6049c299007570542533d8a698b0f7f1070
Mariana <57501545+wari99@users.noreply.github.com>
2021-10-02 18:28:10 -0300

    Update Arco.java

--- PROJETO/27set-01out/figures/Arco.java
+++ PROJETO/27set-01out/figures/Arco.java
@@ -1,47 +1,24 @@
 package figures;
 
 import java.awt.*;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-import java.awt.geom.Arc2D;
-import java.awt.geom.Arc2D.Double;
 
 public class Arco extends Figure {
 	
-    public int inicioangulo, finalangulo; // nao esta no super();
+    public int inicioangulo, finalangulo; 
 
-    public Arco (int x, int y, int w, int h, int r, int g, int b,int rC, int gC, int bC, int inicioangulo, int finalangulo) {
-		super(x,y,w,h,r,g,b,rC,gC,bC);
-
-		this.x = x;
-		this.y = y;
-		this.w = w;
-		this.h = h;
-		
-		this.r = r;
-		this.g = g;
-		this.b = b;
-
-		this.rC = rC;
-		this.bC = bC;
-		this.gC = gC;
-		
+    public Arco (int x, int y, int w, int h, Color contorno, Color fundo, int inicioangulo, int finalangulo) {
+		super(x,y,w,h,contorno,fundo);
         this.inicioangulo = inicioangulo;
         this.finalangulo = finalangulo;
-
     }
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        
-        g2d.setColor(new Color(this.r,this.g,this.b));
+		
+        g2d.setColor(fundo);
         g2d.fillArc(this.x,this.y, this.w,this.h, this.inicioangulo, this.finalangulo);
       
-        g2d.setColor(new Color(this.rC,this.gC,this.bC));
+        g2d.setColor(contorno);
         g2d.drawArc(this.x,this.y, this.w,this.h, this.inicioangulo, this.finalangulo);
     }
 }
