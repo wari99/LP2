814e81033db67c38c5eb7ecc2695cc2bb04b0c3e
Mariana <57501545+wari99@users.noreply.github.com>
2021-11-09 16:53:36 -0300

    Update Rect.java

--- PrimeiroProjeto/figures/Rect.java
+++ PrimeiroProjeto/figures/Rect.java
@@ -8,13 +8,18 @@ public class Rect extends Figure{
 		super(x,y,w,h,contorno,fundo);
     }
 
-    public void paint (Graphics g) {
+    public void paint (Graphics g, boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
-        
+ 		if (focused) {
+		    g2d.setColor(Color.RED);
+		    g2d.drawRect(this.x-1, this.y-1, this.w+2, this.h+2);			
+		}
+		       
 		g2d.setColor(fundo);
 		g2d.fillRect(this.x, this.y, this.w, this.h);
 		
 		g2d.setColor(contorno);
 		g2d.drawRect(this.x,this.y, this.w,this.h);
+    
 	}
 }
