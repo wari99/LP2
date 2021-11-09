ca7dab8450f6ba03b42abd0f0019aeb6c57e061c
Mariana <57501545+wari99@users.noreply.github.com>
2021-11-09 16:53:49 -0300

    Update Arco.java

--- PrimeiroProjeto/figures/Arco.java
+++ PrimeiroProjeto/figures/Arco.java
@@ -12,9 +12,13 @@ public class Arco extends Figure {
         this.finalangulo = finalangulo;
     }
 
-    public void paint (Graphics g) {
+    public void paint (Graphics g, boolean focused) {
         Graphics2D g2d = (Graphics2D) g;
-		
+		if (focused) {
+		    g2d.setColor(Color.RED);
+		    g2d.drawRect(this.x-1, this.y-1, this.w+2, this.h+2);			
+		}
+				
         g2d.setColor(fundo);
         g2d.fillArc(this.x,this.y, this.w,this.h, this.inicioangulo, this.finalangulo);
       
